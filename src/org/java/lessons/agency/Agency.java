d48982161fe930af7c28e9ab9c952dd2f0481d84
NikolGraminova <nbackup619@gmail.com>
2023-07-27 14:19:19 +0200

    Created project workspace

---
+++ src/org/java/lessons/agency/Agency.java
@@ -0,0 +1,26 @@
+package org.java.lessons.agency;
+
+/*
+Consegna Nel progetto java-travel-agency, creare la classe Vacanza caratterizzata da:
+- destinazione
+- data inizio
+- data fine
+Quando viene creata una nuova Vacanza vanno effettuati dei controlli:
+- destinazione, data inizio e data fine non possono essere null
+- la data inizio non può essere già passata
+- la data fine non può essere prima della data inizio
+Se fallisce la validazione vanno sollevate opportune eccezioni
+La classe Vacanza espone un metodo per calcolare la durata in giorni della vacanza.
+Aggiungere una classe Agenzia con metodo main, dove chiediamo all’operatore se vuole inserire una nuova vacanza o uscire.
+Se vuole proseguire con l’inserimento va chiesta la destinazione, il giorno, mese e anno di partenza e il giorno, mese anno di ritorno.
+Con questi dati va generata una nuova vacanza e in console verrà stampato un messaggio del tipo:
+“Hai prenotato una vacanza di [numero di giorni] giorni a [destinazione] dal [data inizio formattata] al [data fine formattata]“.
+Se la creazione della vacanza genera un errore, il programma non deve interrompersi ma va gestito con dei messaggi di errore
+che permettono all’utente di capire cosa è andato storto e di ripetere l’inserimento.
+ */
+
+public class Agency {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/java/lessons/agency/Holiday.java
@@ -0,0 +1,15 @@
+package org.java.lessons.agency;
+
+public class Holiday {
+
+    // FIELDS
+
+
+    // CONSTRUCTORS
+
+
+    // GETTERS AND SETTERS
+
+
+    // METHODS
+}
